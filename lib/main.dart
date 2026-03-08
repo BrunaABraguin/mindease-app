e3634f5e6b410f7c8af7124467276107f512020f
Bruna Braguin <bruna.a.braguin@gmail.com>
2026-03-08 00:50:08 -0300

    Remove redundant env file movement steps from CI workflows and initialize Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mindease_app/firebase_options.dart';
 import 'package:mindease_app/src/app/navigator.dart';
 import 'package:mindease_app/src/data/repositories/timer_repository.dart'
     as repo;
@@ -9,6 +11,7 @@ import 'package:mindease_app/theme.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: "env");
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Bloc.observer = const AppBlocObserver();
   runApp(const App());
 }
